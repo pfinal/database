14a7e70962807ce1d8c882fd5e30ce4071f1c00e
ethan <nicesoft333@qq.com>
2018-01-16 21:38:54 +0800

    exception

--- src/Exception.php
+++ src/Exception.php
@@ -2,13 +2,9 @@
 
 namespace PFinal\Database;
 
-/**
- * Database Exception
- * @since   1.0
- */
 class Exception extends \LogicException
 {
-    public function __construct($message = "Database Exception", $code = 500, $previous = null)
+    public function __construct($message = "Database Exception", $code = 0, $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
--- src/NotFoundException.php
+++ src/NotFoundException.php
@@ -4,7 +4,7 @@ namespace PFinal\Database;
 
 class NotFoundException extends Exception
 {
-    public function __construct($message = "DatabaseNotFoundException", $code = 500, $previous = null)
+    public function __construct($message = "Data Not Found Exception", $code = 0, $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
